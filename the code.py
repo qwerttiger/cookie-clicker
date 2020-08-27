077c308bdab4e86a0d8aaf258f19b4685fe492e9
qwerttiger1 <qwerttiger123456789@gmail.com>
2020-08-27 19:12:15 -0400

    centre the cookie and also add the cookie text

--- the code.py
+++ the code.py
@@ -4,19 +4,23 @@ screen=pygame.display.set_mode([700,700])
 pygame.display.set_caption("Cookie Clicker")
 big_cookie=pygame.image.load("pictures/cookie.png")
 white=(255,255,255)
+black=(0,0,0)
 big_cookie_mask=pygame.mask.from_surface(big_cookie)
 pointer_mask=pygame.mask.Mask((1,1),True)
-screen.fill(white)
-screen.blit(big_cookie,(75,225))
 cookies=0
+font=pygame.font.SysFont("arial",30)
 while True:
+  screen.fill(white)
+  cookies_text=font.render(f"{cookies} cookies",False,black)
+  screen.blit(cookies_text,(350-cookies_text.get_width()/2,100-cookies_text.get_height()/2))
+  screen.blit(big_cookie,(225,225))
   for event in pygame.event.get():
     if event.type==pygame.QUIT:
       pygame.quit()
       sys.exit()
     if event.type==pygame.MOUSEBUTTONDOWN:
       mouse_pos=pygame.mouse.get_pos()
-      if big_cookie_mask.overlap_area(pointer_mask,(mouse_pos[0]-75,mouse_pos[1]-225)):
+      if big_cookie_mask.overlap_area(pointer_mask,(mouse_pos[0]-225,mouse_pos[1]-225)):
         cookies+=1
         print(cookies)
   pygame.display.update()
