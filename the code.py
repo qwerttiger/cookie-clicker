19f5a49daa87b67a0c3a979d18a481258f3c39b0
qwerttiger1 <qwerttiger123456789@gmail.com>
2020-08-27 18:57:10 -0400

    there's the main thing for clicking the cookie

--- the code.py
+++ the code.py
@@ -1,5 +1,22 @@
-import pygame
+import pygame,sys
 pygame.init()
 screen=pygame.display.set_mode([700,700])
 pygame.display.set_caption("Cookie Clicker")
 big_cookie=pygame.image.load("pictures/cookie.png")
+white=(255,255,255)
+big_cookie_mask=pygame.mask.from_surface(big_cookie)
+pointer_mask=pygame.mask.Mask((1,1),True)
+screen.fill(white)
+screen.blit(big_cookie,(75,225))
+cookies=0
+while True:
+  for event in pygame.event.get():
+    if event.type==pygame.QUIT:
+      pygame.quit()
+      sys.exit()
+    if event.type==pygame.MOUSEBUTTONDOWN:
+      mouse_pos=pygame.mouse.get_pos()
+      if big_cookie_mask.overlap_area(pointer_mask,(mouse_pos[0]-75,mouse_pos[1]-225)):
+        cookies+=1
+        print(cookies)
+  pygame.display.update()
