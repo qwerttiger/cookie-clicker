f2c8a41d53cd8038e4d988e7afb3abbddbb33d9c
qwerttiger1 <qwerttiger123456789@gmail.com>
2020-08-28 12:16:05 -0400

    drawtext and draw "Cursor"

--- the code.py
+++ the code.py
@@ -8,14 +8,18 @@ black=(0,0,0)
 big_cookie_mask=pygame.mask.from_surface(big_cookie)
 pointer_mask=pygame.mask.Mask((1,1),True)
 cookies=0
-font=pygame.font.SysFont("arial",30)
 def drawlines():
   for x in range(1,17):
     pygame.draw.rect(screen,black,pygame.Rect(500,41*x-1,200,2))
+def drawtext(text,pos,centre_or_topleft=True,size=30):
+  if not centre_or_topleft:
+    screen.blit(pygame.font.SysFont("arial",size).render(text,True,(0,0,0)),(pos[0]-round(pygame.font.SysFont("arial",size).render(text,True,(0,0,0)).get_width()/2),pos[1]-pygame.font.SysFont("arial",size).render(text,True,(0,0,0)).get_height()/2))
+  else:
+    screen.blit(pygame.font.SysFont("arial",size).render(text,True,(0,0,0)),(pos[0],pos[1]))
 while True:
   screen.fill(white)
-  cookies_text=font.render(f"{cookies} cookies",True,black)
-  screen.blit(cookies_text,(350-cookies_text.get_width()/2,100-cookies_text.get_height()/2))
+  drawtext(f"{cookies} cookies",(350,100),False)
+  drawtext("Cursor",(500,41))
   screen.blit(big_cookie,(225,225))
   pygame.draw.rect(screen,black,pygame.Rect(199,0,2,700))
   pygame.draw.rect(screen,black,pygame.Rect(499,0,2,700))
