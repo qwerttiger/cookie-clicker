93567cbf8d27160a103c60a0caec2ac975753389
qwerttiger1 <qwerttiger123456789@gmail.com>
2020-08-28 15:53:24 -0400

    add underscores and also add sound effects

--- the code.py
+++ the code.py
@@ -17,10 +17,12 @@ click4=pygame.mixer.Sound("sounds/click4.wav")
 click5=pygame.mixer.Sound("sounds/click5.wav")
 click6=pygame.mixer.Sound("sounds/click6.wav")
 click7=pygame.mixer.Sound("sounds/click7.wav")
-def drawlines():
+def play_random_sound():
+  exec(f"click{random.randint(1,7)}.play()")
+def draw_lines():
   for x in range(1,17):
     pygame.draw.rect(screen,black,pygame.Rect(500,41*x-1,200,2))
-def drawtext(text,pos,side=True,size=10):
+def draw_text(text,pos,side=True,size=10):
   if not side:
     screen.blit(pygame.font.SysFont("arial",size).render(text,True,(0,0,0)),(pos[0]-round(pygame.font.SysFont("arial",size).render(text,True,(0,0,0)).get_width()/2),pos[1]-pygame.font.SysFont("arial",size).render(text,True,(0,0,0)).get_height()/2))
   else:
@@ -33,29 +35,29 @@ def add_cookies():
 add_cookies()
 while True:
   screen.fill(white)
-  drawtext(f"{round(cookies)} cookies",(350,100),False,30)
-  drawtext(f"CpS: {cps}",(350,150),False,30)
-  drawtext(f"Cursor, cost (), have ()",(500,20))
-  drawtext(f"Grandma, cost (), have ()",(500,61))
-  drawtext(f"Farm, cost (), have ()",(500,102))
-  drawtext(f"Mine, cost (), have ()",(500,143))
-  drawtext(f"Factory, cost (), have ()",(500,184))
-  drawtext(f"Bank, cost (), have ()",(500,225))
-  drawtext(f"Temple, cost (), have ()",(500,266))
-  drawtext(f"Wizard Tower, cost (), have ()",(500,307))
-  drawtext(f"Shipent, cost (), have ()",(500,348))
-  drawtext(f"Alchemy Lab, cost (), have ()",(500,389))
-  drawtext(f"Portal, cost (), have ()",(500,430))
-  drawtext(f"Time Machine, cost (), have ()",(500,471))
-  drawtext(f"Antimatter Condenser, cost (), have ()",(500,512))
-  drawtext(f"Prism, cost (), have ()",(500,553))
-  drawtext(f"Chancemaker, cost (), have ()",(500,594))
-  drawtext(f"Fractal Engine, cost (), have ()",(500,635))
-  drawtext(f"Javascript Console, cost (), have ()",(500,676))
+  draw_text(f"{round(cookies)} cookies",(350,100),False,30)
+  draw_text(f"CpS: {cps}",(350,150),False,30)
+  draw_text(f"Cursor, cost (), have ()",(500,20))
+  draw_text(f"Grandma, cost (), have ()",(500,61))
+  draw_text(f"Farm, cost (), have ()",(500,102))
+  draw_text(f"Mine, cost (), have ()",(500,143))
+  draw_text(f"Factory, cost (), have ()",(500,184))
+  draw_text(f"Bank, cost (), have ()",(500,225))
+  draw_text(f"Temple, cost (), have ()",(500,266))
+  draw_text(f"Wizard Tower, cost (), have ()",(500,307))
+  draw_text(f"Shipent, cost (), have ()",(500,348))
+  draw_text(f"Alchemy Lab, cost (), have ()",(500,389))
+  draw_text(f"Portal, cost (), have ()",(500,430))
+  draw_text(f"Time Machine, cost (), have ()",(500,471))
+  draw_text(f"Antimatter Condenser, cost (), have ()",(500,512))
+  draw_text(f"Prism, cost (), have ()",(500,553))
+  draw_text(f"Chancemaker, cost (), have ()",(500,594))
+  draw_text(f"Fractal Engine, cost (), have ()",(500,635))
+  draw_text(f"Javascript Console, cost (), have ()",(500,676))
   screen.blit(big_cookie,(225,225))
   pygame.draw.rect(screen,black,pygame.Rect(199,0,2,700))
   pygame.draw.rect(screen,black,pygame.Rect(499,0,2,700))
-  drawlines()
+  draw_lines()
   for event in pygame.event.get():
     if event.type==pygame.QUIT:
       pygame.quit()
@@ -66,4 +68,5 @@ while True:
         mouse_pos=pygame.mouse.get_pos()
         if big_cookie_mask.overlap_area(pointer_mask,(mouse_pos[0]-225,mouse_pos[1]-225)):
           cookies+=1
+          play_random_sound()
   pygame.display.update()
