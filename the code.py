ca09a8a77f0182ed5d35fda1cd338ef49f5f1cbf
qwerttiger1 <qwerttiger123456789@gmail.com>
2020-08-29 12:18:25 -0400

    you can buy all of them, python console instead of javascript console, and also add the console AKA eval() and exec()

--- the code.py
+++ the code.py
@@ -1,6 +1,4 @@
-import sys,threading,os,random,time
-os.environ["PYGAME_HIDE_SUPPORT_PROMPT"]="hide"
-import pygame; from numbershortener import numbershortener as ns; from decimal import Decimal as decimal
+import sys,threading,os,random,time; os.environ["PYGAME_HIDE_SUPPORT_PROMPT"]="hide"; import pygame; from numbershortener import numbershortener as ns; from decimal import Decimal as decimal
 pygame.init()
 screen=pygame.display.set_mode([700,700])
 pygame.display.set_caption("Cookie Clicker")
@@ -22,6 +20,18 @@ buy1=pygame.mixer.Sound("sounds/buy1.wav")
 buy2=pygame.mixer.Sound("sounds/buy2.wav")
 buy3=pygame.mixer.Sound("sounds/buy3.wav")
 buy4=pygame.mixer.Sound("sounds/buy4.wav")
+x=None
+def buy(num):
+  if num*41-41<=mouse_pos[1]<num*41 and cookies>=eval(f"bc{num}"):
+    exec(f"cookies-=bc{num}",globals())
+    exec(f"bc{num}=round(bc{num}*1.15)",globals())
+    exec(f"b{num}+=1",globals())
+    exec(f"cps+=decimal(str(bp{num}))",globals())
+    play_random_buy()
+def clear_save():
+  file=open("save data.txt","w")
+  file.write("0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n15\n100\n1100\n12000\n130000\n1400000\n20000000\n330000000\n5100000000\n75000000000\n1000000000000\n14000000000000\n170000000000000\n2100000000000000\n26000000000000000\n310000000000000000\n71000000000000000000\n0.1\n1\n8\n47\n260\n1400\n7800\n44000\n260000\n1600000\n10000000\n65000000\n430000000\n2900000000\n21000000000\n150000000000\n1100000000000")
+  file.close()
 def load_save_data():
   global b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b16,b17,bc1,bc2,bc3,bc4,bc5,bc6,bc7,bc8,bc9,bc10,bc11,bc12,bc13,bc14,bc15,bc16,bc17,bp1,bp2,bp3,bp4,bp5,bp6,bp7,bp8,bp9,bp10,bp11,bp12,bp13,bp14,bp15,bp16,bp17
   try:
@@ -91,11 +101,11 @@ def play_random_click():
   time.sleep(0.1)
   exec(f"click{a}.play()")
 def play_random_buy():
-  exec(f"buy{random.randint(1,7)}.play()")
+  exec(f"buy{random.randint(1,4)}.play()")
 def draw_lines():
   for x in range(1,17):
     pygame.draw.rect(screen,black,pygame.Rect(500,41*x-1,200,2))
-def draw_text(text,pos,side=True,size=10):
+def draw_text(text,pos,size=10,side=True):
   if not side:
     screen.blit(pygame.font.SysFont("arial",size).render(text,True,(0,0,0)),(pos[0]-round(pygame.font.SysFont("arial",size).render(text,True,(0,0,0)).get_width()/2),pos[1]-pygame.font.SysFont("arial",size).render(text,True,(0,0,0)).get_height()/2))
   else:
@@ -105,12 +115,46 @@ def add_cookies():
   cookies+=decimal(str(cps))/10
   t=threading.Timer(0.1,add_cookies)
   t.start()
+def unblocker():
+  while not finish:
+    for event in pygame.event.get():
+      if event.type==pygame.QUIT:
+        pygame.quit()
+        t.cancel()
+        sys.exit()
+def inputcommand():
+  global x
+  x=input("Command (\"quit\" to quit): ").lower()
+def command():
+  global loop,x,finish
+  finish=False
+  del x
+  threading.Thread(target=inputcommand).start()
+  ok=False
+  while not ok:
+    try:
+      x
+    except:
+      pass
+    else:
+      ok=True
+  if x in ["quit","exit"]:
+    loop=False
+  else:
+    try:
+      print("Output:",eval(x,globals())) if eval(x,globals())!=None else exec(x,globals())
+    except:
+      try:
+        exec(x,globals())
+      except Exception as e:
+        print(f"Error: {e}")
+  finish=True
 add_cookies()
 load_save_data()
 while True:
   screen.fill(white)
-  draw_text(f"{round(cookies)} cookies",(350,100),False,30)
-  draw_text(f"CpS: {cps}",(350,150),False,30)
+  draw_text(f"{round(cookies)} cookies",(350,100),30,False)
+  draw_text(f"CpS: {cps}",(350,150),20,False)
   draw_text(f"Cursor, cost {ns(bc1)}, have {ns(b1)}",(500,20))
   draw_text(f"Grandma, cost {ns(bc2)}, have {ns(b2)}",(500,61))
   draw_text(f"Farm, cost {ns(bc3)}, have {ns(b3)}",(500,102))
@@ -127,10 +171,12 @@ while True:
   draw_text(f"Prism, cost {ns(bc14)}, have {ns(b14)}",(500,553))
   draw_text(f"Chancemaker, cost {ns(bc15)}, have {ns(b15)}",(500,594))
   draw_text(f"Fractal Engine, cost {ns(bc16)}, have {ns(b16)}",(500,635))
-  draw_text(f"Javascript Console, cost {ns(bc17)}, have {ns(b17)}",(500,676))
+  draw_text(f"Python Console, cost {ns(bc17)}, have {ns(b17)}",(500,676))
+  draw_text("Console",(350,550),25,False)
   screen.blit(big_cookie,(225,225))
   pygame.draw.rect(screen,black,pygame.Rect(199,0,2,700))
   pygame.draw.rect(screen,black,pygame.Rect(499,0,2,700))
+  pygame.draw.rect(screen,black,pygame.Rect(300,500,100,100),1)
   draw_lines()
   for event in pygame.event.get():
     if event.type==pygame.QUIT:
@@ -144,10 +190,13 @@ while True:
           cookies+=1
           threading.Thread(target=play_random_click).start()
         if mouse_pos[0]>=500:
-          if mouse_pos[1]<41 and cookies>=bc1:
-            bc1=round(bc1*1.15)
-            b1+=1
-            cps+=decimal(str(bp1))
-            cookies-=bc1
-            play_random_buy()
+          for _ in range(1,18):
+            buy(_)
+        if 300<=mouse_pos[0]<=400 and 500<=mouse_pos[1]<=600:
+          loop=True
+          while loop:
+            finish=False
+            threading.Thread(target=command).start()
+            unblocker()
+
   pygame.display.update()
