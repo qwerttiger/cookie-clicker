d429d82c99b05a0c78cabe8d634953d8f5790402
qwerttiger1 <qwerttiger123456789@gmail.com>
2020-08-29 17:03:33 -0400

    comments and spaces

--- the code.py
+++ the code.py
@@ -1,44 +1,65 @@
-import sys,threading,os,random,time; os.environ["PYGAME_HIDE_SUPPORT_PROMPT"]="hide"; import pygame; from numbershortener import numbershortener as ns; from decimal import Decimal as decimal
-pygame.init()
-screen=pygame.display.set_mode([700,700])
-pygame.display.set_caption("Cookie Clicker")
-big_cookie=pygame.image.load("pictures/cookie.png")
-white=(255,255,255)
-black=(0,0,0)
-big_cookie_mask=pygame.mask.from_surface(big_cookie)
-pointer_mask=pygame.mask.Mask((1,1),True)
-click1=pygame.mixer.Sound("sounds/click1.wav")
-click2=pygame.mixer.Sound("sounds/click2.wav")
-click3=pygame.mixer.Sound("sounds/click3.wav")
-click4=pygame.mixer.Sound("sounds/click4.wav")
-click5=pygame.mixer.Sound("sounds/click5.wav")
-click6=pygame.mixer.Sound("sounds/click6.wav")
-click7=pygame.mixer.Sound("sounds/click7.wav")
-buy1=pygame.mixer.Sound("sounds/buy1.wav")
-buy2=pygame.mixer.Sound("sounds/buy2.wav")
-buy3=pygame.mixer.Sound("sounds/buy3.wav")
-buy4=pygame.mixer.Sound("sounds/buy4.wav")
-x=None
-def buy(num):
-  if num*41-41<=mouse_pos[1]<num*41 and cookies>=eval(f"bc{num}"):
-    exec(f"cookies-=bc{num}",globals())
-    exec(f"bc{num}=round(bc{num}*1.15)",globals())
-    exec(f"b{num}+=1",globals())
-    exec(f"cps+=decimal(str(bp{num}))",globals())
-    play_random_buy()
-def clear_save():
-  file=open("save data.txt","w")
-  file.write("0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n15\n100\n1100\n12000\n130000\n1400000\n20000000\n330000000\n5100000000\n75000000000\n1000000000000\n14000000000000\n170000000000000\n2100000000000000\n26000000000000000\n310000000000000000\n71000000000000000000\n0.1\n1\n8\n47\n260\n1400\n7800\n44000\n260000\n1600000\n10000000\n65000000\n430000000\n2900000000\n21000000000\n150000000000\n1100000000000\n0\n0")
-  file.close()
-def load_save_data():
-  global b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b16,b17,bc1,bc2,bc3,bc4,bc5,bc6,bc7,bc8,bc9,bc10,bc11,bc12,bc13,bc14,bc15,bc16,bc17,bp1,bp2,bp3,bp4,bp5,bp6,bp7,bp8,bp9,bp10,bp11,bp12,bp13,bp14,bp15,bp16,bp17,cookies,cps
-  try:
-    file=open("save data.txt").read().split()
-  except:
-    file=open("save data.txt","w")
-    file.write("0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n15\n100\n1100\n12000\n130000\n1400000\n20000000\n330000000\n5100000000\n75000000000\n1000000000000\n14000000000000\n170000000000000\n2100000000000000\n26000000000000000\n310000000000000000\n71000000000000000000\n0.1\n1\n8\n47\n260\n1400\n7800\n44000\n260000\n1600000\n10000000\n65000000\n430000000\n2900000000\n21000000000\n150000000000\n1100000000000\n0\n0")
-    file.close()
-    file=open("save data.txt").read().split()
+import sys #import sys to exit
+import threading #import threading for timers and threads
+import random #import random for random numbers
+import time #import time to sleep
+import os #import os to hide support prompt
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"]="hide" #hide support prompt
+import pygame #import pygame
+from numbershortener import numbershortener as ns #import numbershortener to shorten numbers
+from decimal import Decimal as decimal #import decimal numbers with infinite precision
+
+pygame.init() #initiate pygame
+screen=pygame.display.set_mode([700,700]) #make screen
+pygame.display.set_caption("Cookie Clicker") #set caption
+
+white=(255,255,255) #set white colour
+black=(0,0,0) #set black colour
+
+pointer_mask=pygame.mask.Mask((1,1),True) #pointer mask
+big_cookie=pygame.image.load("pictures/cookie.png") #big cookie picture
+big_cookie_mask=pygame.mask.from_surface(big_cookie) #big cookie mask
+
+click1=pygame.mixer.Sound("sounds/click1.wav") #click sound 1
+click2=pygame.mixer.Sound("sounds/click2.wav") #click sound 2
+click3=pygame.mixer.Sound("sounds/click3.wav") #click sound 3
+click4=pygame.mixer.Sound("sounds/click4.wav") #click sound 4
+click5=pygame.mixer.Sound("sounds/click5.wav") #click sound 5
+click6=pygame.mixer.Sound("sounds/click6.wav") #click sound 6
+click7=pygame.mixer.Sound("sounds/click7.wav") #click sound 7
+buy1=pygame.mixer.Sound("sounds/buy1.wav") #buy sound 1
+buy2=pygame.mixer.Sound("sounds/buy2.wav") #buy sound 2
+buy3=pygame.mixer.Sound("sounds/buy3.wav") #buy sound 3
+buy4=pygame.mixer.Sound("sounds/buy4.wav") #buy sound 4
+
+x=None #set x to be nothing
+
+def buy(num): #buy building number num
+  if num*41-41<=mouse_pos[1]<num*41 and cookies>=eval(f"bc{num}"): #if you click on it and you can buy it
+    exec(f"cookies-=bc{num}",globals()) #decrease your cookies
+    exec(f"bc{num}=round(bc{num}*1.15)",globals()) #increase the price
+    exec(f"b{num}+=1",globals()) #add 1 to bought
+    exec(f"cps+=decimal(str(bp{num}))",globals()) #add the cps to your cps
+    
+    play_random_buy() #play the buy sound
+
+def clear_save(): #clear save
+  file=open("save data.txt","w") #open the file
+  file.write("0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n15\n100\n1100\n12000\n130000\n1400000\n20000000\n330000000\n5100000000\n75000000000\n1000000000000\n14000000000000\n170000000000000\n2100000000000000\n26000000000000000\n310000000000000000\n71000000000000000000\n0.1\n1\n8\n47\n260\n1400\n7800\n44000\n260000\n1600000\n10000000\n65000000\n430000000\n2900000000\n21000000000\n150000000000\n1100000000000\n0\n0") #write to file
+  file.close() #close file
+  
+  load_save_data() #load save data
+
+def load_save_data(): #load save data
+  global b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b16,b17,bc1,bc2,bc3,bc4,bc5,bc6,bc7,bc8,bc9,bc10,bc11,bc12,bc13,bc14,bc15,bc16,bc17,bp1,bp2,bp3,bp4,bp5,bp6,bp7,bp8,bp9,bp10,bp11,bp12,bp13,bp14,bp15,bp16,bp17,cookies,cps #global variables
+  try: #try to
+    file=open("save data.txt").read().split() #open file for reading
+  except: #if the file does not exist
+    file=open("save data.txt","w") #open file for writing
+    file.write("0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n15\n100\n1100\n12000\n130000\n1400000\n20000000\n330000000\n5100000000\n75000000000\n1000000000000\n14000000000000\n170000000000000\n2100000000000000\n26000000000000000\n310000000000000000\n71000000000000000000\n0.1\n1\n8\n47\n260\n1400\n7800\n44000\n260000\n1600000\n10000000\n65000000\n430000000\n2900000000\n21000000000\n150000000000\n1100000000000\n0\n0") #write this
+    file.close() #close file
+    file=open("save data.txt").read().split() #read file
+
+  #set the variables
   b1=int(file[0])
   b2=int(file[1])
   b3=int(file[2])
@@ -92,72 +113,93 @@ def load_save_data():
   bp17=float(file[50])
   cookies=decimal(file[51])
   cps=decimal(file[52])
-def play_random_click():
-  a=random.randint(1,7)
-  exec(f"click{a}.play()")
-  a+=random.randint(1,5)
-  a=a%7
-  a=7 if a==0 else a
-  time.sleep(0.1)
-  exec(f"click{a}.play()")
-def play_random_buy():
-  exec(f"buy{random.randint(1,4)}.play()")
-def draw_lines():
-  for x in range(1,17):
-    pygame.draw.rect(screen,black,pygame.Rect(500,41*x-1,200,2))
-def draw_text(text,pos,size=8,side=True):
-  if not side:
-    screen.blit(pygame.font.SysFont("arial",size).render(text,True,(0,0,0)),(pos[0]-round(pygame.font.SysFont("arial",size).render(text,True,(0,0,0)).get_width()/2),pos[1]-pygame.font.SysFont("arial",size).render(text,True,(0,0,0)).get_height()/2))
-  else:
-    screen.blit(pygame.font.SysFont("arial",size).render(text,True,(0,0,0)),(pos[0],pos[1]-pygame.font.SysFont("arial",size).render(text,True,(0,0,0)).get_height()/2))
-def add_cookies():
-  global cookies,t
-  cookies+=decimal(str(cps))/10
-  t=threading.Timer(0.1,add_cookies)
-  t.start()
-def unblocker():
-  while not finish:
-    for event in pygame.event.get():
-      if event.type==pygame.QUIT:
-        pygame.quit()
-        t.cancel()
-        sys.exit()
-def inputcommand():
-  global x
-  x=input("Command (\"quit\" to quit): ").lower()
-def command():
-  global loop,x,finish
-  finish=False
-  del x
-  threading.Thread(target=inputcommand).start()
-  ok=False
-  while not ok:
-    try:
-      x
-    except:
-      pass
-    else:
-      ok=True
-  if x in ["quit","exit"]:
-    loop=False
-  else:
-    try:
-      print("Output:",eval(x,globals())) if eval(x,globals())!=None else exec(x,globals())
-    except:
-      try:
-        exec(x,globals())
-      except Exception as e:
-        print(f"Error: {e}")
-  finish=True
-load_save_data()
-add_cookies()
-while True:
-  screen.fill(white)
-  if cookies!=decimal("infinity"):
-    draw_text(f"{ns(round(cookies))} cookies",(350,100),25,False)
-  else:
-    draw_text(f"{ns(decimal('infinity'))} cookies",(350,100),25,False)
-  draw_text(f"CpS: {ns(cps)}",(350,150),18,False)
+
+def play_random_click(): #play random click sound
+  a=random.randint(1,7) #set a to a random number
+  exec(f"click{a}.play()") #play sound
+  a+=random.randint(1,5) #add
+  a=7 if a==7 else a%7 #adjust to a number that make sense
+  time.sleep(0.1) #wait a little bit
+  exec(f"click{a}.play()") #play sound
+
+def play_random_buy(): #play random buy sound
+  exec(f"buy{random.randint(1,4)}.play()") #play sound
+
+def draw_lines(): #draw these lines
+  for x in range(1,17): #for every number in here
+    pygame.draw.rect(screen,black,pygame.Rect(500,41*x-1,200,2)) #draw a line corresponding to the number
+
+def draw_text(text,pos,size=8,side=True): #draw text
+  if not side: #if not side
+    screen.blit(pygame.font.SysFont("arial",size).render(text,True,(0,0,0)),(pos[0]-round(pygame.font.SysFont("arial",size).render(text,True,(0,0,0)).get_width()/2),pos[1]-pygame.font.SysFont("arial",size).render(text,True,(0,0,0)).get_height()/2)) #draw it
+  else: #if side
+    screen.blit(pygame.font.SysFont("arial",size).render(text,True,(0,0,0)),(pos[0],pos[1]-pygame.font.SysFont("arial",size).render(text,True,(0,0,0)).get_height()/2)) #draw it
+
+def add_cookies(): #add cookies
+  global cookies,t #global variables
+  
+  cookies+=decimal(str(cps))/10 #add cookies
+  
+  t=threading.Timer(0.1,add_cookies) #t is a timer
+  t.start() #start timer
+
+def unblocker(): #unblocker
+  while not finish: #while not finish
+    for event in pygame.event.get(): #for every event
+      if event.type==pygame.QUIT: #if quit pygame
+        pygame.quit() #pygame quit
+        t.cancel() #cancel timer
+        sys.exit() #exit
+
+def inputcommand(): #input
+  global x #global x
+  x=input("Command (\"quit\" to quit): ").lower() #input
+
+def command(): #command
+  global loop,x,finish #global variables
+  
+  finish=False #set finish to false
+  
+  del x #delete x
+  threading.Thread(target=inputcommand).start() #find x
+  
+  ok=False #set ok to be false
+  while not ok: #while you don't know x
+    try: #try to
+      x #find x
+    except: #if x is not defined
+      pass #pass
+    else: #then,
+      ok=True #ok is true
+  
+  if x in ["quit","exit"]: #if x is exit or quit
+    loop=False #break out of loop
+  else: #else
+    try: #try to
+      print("Output:",eval(x,globals())) if eval(x,globals())!=None else exec(x,globals()) #output
+    except: #if that didn't work
+      try: #then
+        exec(x,globals()) #do this
+      except Exception as e: #if THAT didn't work
+        print(f"Error: {e}") #then print error message
+  
+  finish=True #finish
+
+#real game starts here
+load_save_data() #load save data
+add_cookies() #add cookies
+
+while True: #game loop
+  screen.fill(white) #fill screen with white
+  
+  if cookies!=decimal("infinity"): #if not infinity cookies
+    draw_text(f"{ns(round(cookies))} cookies",(350,100),25,False) #draw text
+  else: #if infinity cookies
+    draw_text(f"{ns(decimal('infinity'))} cookies",(350,100),25,False) #draw text
+  
+  draw_text(f"CpS: {ns(cps)}",(350,150),18,False) #draw cps
+
+  #these are all building text
   draw_text(f"Cursor, cost {ns(bc1)}, have {b1}",(500,20))
   draw_text(f"Grandma, cost {ns(bc2)}, have {b2}",(500,61))
   draw_text(f"Farm, cost {ns(bc3)}, have {b3}",(500,102))
@@ -175,34 +217,45 @@ while True:
   draw_text(f"Chancemaker, cost {ns(bc15)}, have {b15}",(500,594))
   draw_text(f"Fractal Engine, cost {ns(bc16)}, have {b16}",(500,635))
   draw_text(f"Python Console, cost {ns(bc17)}, have {b17}",(500,676))
-  draw_text("Console",(350,550),25,False)
-  screen.blit(big_cookie,(225,225))
-  pygame.draw.rect(screen,black,pygame.Rect(199,0,2,700))
-  pygame.draw.rect(screen,black,pygame.Rect(499,0,2,700))
-  pygame.draw.rect(screen,black,pygame.Rect(300,500,100,100),1)
-  draw_lines()
-  for event in pygame.event.get():
-    if event.type==pygame.QUIT:
-      pygame.quit()
-      t.cancel()
-      sys.exit()
-    if event.type==pygame.MOUSEBUTTONDOWN:
-      if event.button in [1,2,3]:
-        mouse_pos=pygame.mouse.get_pos()
-        if big_cookie_mask.overlap_area(pointer_mask,(mouse_pos[0]-225,mouse_pos[1]-225)):
-          cookies+=1
-          threading.Thread(target=play_random_click).start()
-        if mouse_pos[0]>=500:
-          for _ in range(1,18):
-            buy(_)
-        if 300<=mouse_pos[0]<=400 and 500<=mouse_pos[1]<=600:
-          loop=True
-          while loop:
-            finish=False
-            threading.Thread(target=command).start()
-            unblocker()
-    if event.type==pygame.KEYDOWN:
-      if event.key==pygame.K_s and (event.mod & pygame.KMOD_CTRL):
-        open("save data.txt","w").write(eval("chr(10).join([str(x) for x in [b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b16,b17,bc1,bc2,bc3,bc4,bc5,bc6,bc7,bc8,bc9,bc10,bc11,bc12,bc13,bc14,bc15,bc16,bc17,bp1,bp2,bp3,bp4,bp5,bp6,bp7,bp8,bp9,bp10,bp11,bp12,bp13,bp14,bp15,bp16,bp17,cookies,cps]])"))
+  
+  draw_text("Command Line",(350,550),15,False) #command line text
+  
+  screen.blit(big_cookie,(225,225)) #draw big cookie
+  
+  pygame.draw.rect(screen,black,pygame.Rect(199,0,2,700)) #draw filler 1
+  pygame.draw.rect(screen,black,pygame.Rect(499,0,2,700)) #draw filler 2
+  
+  pygame.draw.rect(screen,black,pygame.Rect(300,500,100,100),1) #draw command line box
+  
+  draw_lines() #draw lines
+  
+  for event in pygame.event.get(): #for every event
+    if event.type==pygame.QUIT: #if quit pygame
+      pygame.quit() #pygame quit
+      t.cancel() #cancel timer
+      sys.exit() #exit
+    if event.type==pygame.MOUSEBUTTONDOWN: #if you click
+      if event.button in [1,2,3]: #if you click and not scroll
+        mouse_pos=pygame.mouse.get_pos() #set mouse_pos to mouse position
+        
+        if big_cookie_mask.overlap_area(pointer_mask,(mouse_pos[0]-225,mouse_pos[1]-225)): #if you click the big cookie
+          cookies+=1 #add 1 to cookies
+          threading.Thread(target=play_random_click).start() #play random click sound
+        
+        if mouse_pos[0]>=500: #if you buy
+          for _ in range(1,18): #for everything in the range
+            buy(_) #see if you bought it
+        
+        if 300<=mouse_pos[0]<=400 and 500<=mouse_pos[1]<=600: #if click console
+          loop=True #loop is true
+          
+          while loop: #while loop
+            finish=False #finish is false
+            threading.Thread(target=command).start() #start command line
+            unblocker() #unblock
+    
+    if event.type==pygame.KEYDOWN: #if click down
+      if event.key==pygame.K_s and (event.mod & pygame.KMOD_CTRL): #if you press ctrl-s
+        open("save data.txt","w").write(eval("chr(10).join([str(x) for x in [b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,b11,b12,b13,b14,b15,b16,b17,bc1,bc2,bc3,bc4,bc5,bc6,bc7,bc8,bc9,bc10,bc11,bc12,bc13,bc14,bc15,bc16,bc17,bp1,bp2,bp3,bp4,bp5,bp6,bp7,bp8,bp9,bp10,bp11,bp12,bp13,bp14,bp15,bp16,bp17,cookies,cps]])")) #save
 
-  pygame.display.update()
+  pygame.display.update() #update pygame
